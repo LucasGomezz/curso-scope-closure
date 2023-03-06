8be1fc0058384e4183901ead21223912a77ff723
LucasGomezz <lucasgvilla@hotmail.com>
2023-03-06 18:04:13 -0300

    Clousure y su ambito lexico

---
+++ src/clousure/clousure.js
@@ -0,0 +1,5 @@
+//Clousure
+//Permite acceder al ambito de una funcion exterior desde una funcion interior.En js, las clausuras se crean cada vez que una funcion es creada.
+//No son siempre utilizados
+//Tienes clousure cuando una funcion cualquiera accede a una variable fuera de su contexto
+
