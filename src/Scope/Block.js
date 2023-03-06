a6c2c1d92b8180ddef9a6161754209369df2e085
LucasGomezz <lucasgvilla@hotmail.com>
2023-03-06 16:29:33 -0300

    Block Scope

---
+++ src/Scope/Block.js
@@ -0,0 +1,15 @@
+//Block Scope Se puede usar las variables dentro del bloque entre {}
+function fruits(){
+    if(true){
+        var fruit1 = 'Apple'; //Function Scope
+        let fruit2 = 'Kiwi'; //Block Scope
+        const fruit3 = 'Banana'; //Block Scope
+        console.log(fruit2);
+        console.log(fruit3);
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruits();
