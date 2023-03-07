0a30285aab006929b6b19a603045a36d262fae20
LucasGomezz <lucasgvilla@hotmail.com>
2023-03-07 15:50:16 -0300

    Debuggin

---
+++ src/debug/index.js
@@ -0,0 +1,11 @@
+//Debuggin
+//Sirve para probar los errores pausando la ejecucion dentro del inspector
+var a = 'Hello';
+
+function hello(){
+    let b = 'Hello World';
+    const c = 'Hello World!'
+    debugger;
+}
+
+hello();
--- src/hoisting/inex.js
+++ src/hoisting/inex.js
@@ -1,4 +1,6 @@
 //Hoisting
+// Hoisting es un término para describir que las declaraciones de variables y funciones son desplazadas a la parte superior del scope más cercano, scope global o de función.
+// Esto sucede solamente con las declaraciones y no con las asignaciones.
 var nameOfDog; //Undifined
 
 console.log(nameOfDog);
